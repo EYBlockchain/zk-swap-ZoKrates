7d6409ed9a5f29d4ffe64940b26a1a2068cf188f
Chaitanya Konda <ckonda@uk.ey.com>
2019-09-23 17:28:39 +0100

    fix

--- zokrates_core/lib/gm17.cpp
+++ zokrates_core/lib/gm17.cpp
@@ -16,6 +16,7 @@
 #include "libff/algebra/curves/mnt/mnt4/mnt4_pp.hpp"
 #include "libff/algebra/curves/mnt/mnt6/mnt6_pp.hpp"
 #include "libff/algebra/curves/bls12_377/bls12_377_pp.hpp"
+#include "libff/algebra/curves/sw6/sw6_pp.hpp"
 // contains required interfaces and types (keypair, proof, generator, prover, verifier)
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_se_ppzksnark/r1cs_se_ppzksnark.hpp>
 
